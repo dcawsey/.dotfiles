aa8381e1f23a5914cc03777b8908e4a061c05b7d
Dylan Cawsey <dylan.cawsey@service.nsw.gov.au>
2023-07-27 17:16:10 +1000

    Added Harpoon plugin + fixed config to be 4 spaces for tabs

---
+++ nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,73 @@
+return {
+    {
+        "ThePrimeagen/harpoon",
+        opts = { desc = "harpoon" },
+        keys = function()
+            return {
+                {
+                    "<leader>ha",
+                    function()
+                        require("harpoon.mark").add_file()
+                    end,
+                    desc = "Add mark",
+                },
+                {
+                    "<leader>hm",
+                    function()
+                        require("harpoon.ui").toggle_quick_menu()
+                    end,
+                    desc = "Toggle menu",
+                },
+                {
+                    "<leader>hn",
+                    function()
+                        require("harpoon.ui").nav_next()
+                    end,
+                    desc = "Navigate to next mark",
+                },
+                {
+                    "<leader>hp",
+                    function()
+                        require("harpoon.ui").nav_prev()
+                    end,
+                    desc = "Navigate to previous mark",
+                },
+                {
+                    "<leader>h1",
+                    function()
+                        require("harpoon.ui").nav_file(1)
+                    end,
+                    desc = "Navigate to mark 1",
+                },
+                {
+                    "<leader>h2",
+                    function()
+                        require("harpoon.ui").nav_file(2)
+                    end,
+                    desc = "Navigate to mark 2",
+                },
+                {
+                    "<leader>h3",
+                    function()
+                        require("harpoon.ui").nav_file(3)
+                    end,
+                    desc = "Navigate to mark 3",
+                },
+                {
+                    "<leader>h4",
+                    function()
+                        require("harpoon.ui").nav_file(4)
+                    end,
+                    desc = "Navigate to mark 4",
+                },
+                {
+                    "<leader>h5",
+                    function()
+                        require("harpoon.ui").nav_file(5)
+                    end,
+                    desc = "Navigate to mark 5",
+                },
+            }
+        end,
+    },
+}
