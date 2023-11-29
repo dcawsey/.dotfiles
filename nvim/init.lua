71a61677d1845beb08da6ef0971f7a8ee2e2f4d4
Dylan Cawsey <dylancawsey@gmail.com>
2023-11-30 10:54:04 +1100

    Added telescope config to ignore node_modules + include dot files when searching files

--- nvim/init.lua
+++ nvim/init.lua
@@ -166,6 +166,14 @@ require('lazy').setup({
         end,
       },
     },
+    opts = {
+      pickers = {
+        find_files = {
+          hidden = true,
+          file_ignore_patterns = { 'node_modules' }
+        }
+      }
+    }
   },
 
   {
