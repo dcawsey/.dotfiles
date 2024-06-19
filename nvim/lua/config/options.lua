0c52a64a2262650c72a64000341294060a3c2d08
Dylan Cawsey <dylancawsey@gmail.com>
2024-06-19 21:00:15 +1000

    Setup simple directory structure for nvim

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,6 @@
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+vim.g.maplocalleader = "\\" -- Same for `maplocalleader`
+
+local opt = vim.opt
+
+opt.clipboard = "unnamedplus"
